84037af497057b16cbfab818a3e191ff0aba7acd
zane <39070793+zaneenders@users.noreply.github.com>
2024-10-08 20:13:23 -0600

    init

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "herbie-server-thrasher",
+    targets: [
+        .executableTarget(
+            name: "herbie-server-thrasher"),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Herbie server thrasher
+
+A tool to trash and analyze the Herbie server.
